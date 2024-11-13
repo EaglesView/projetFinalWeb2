48a4b406f7d7374274b1f97519d6809f2dfd8448
Jean-Marc B. <bouchardplatre@gmail.com>
2024-11-13 10:45:32 -0500

    initial commit

---
+++ src/core/engine/engine.js
@@ -0,0 +1,5 @@
+// Fichier principal du moteur de jeu
+// Permet de lier toutes les phases du projet ensemble
+// ***********************************************
+// Auteur : Jean-Marc Bouchard
+// ***********************************************
